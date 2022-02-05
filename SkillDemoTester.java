f9d455cb0414b02efcebea90769ed3db8b9a04c6
kenva <ken.vandeventer@outlook.com>
2022-02-04 18:40:51 -0800

    libs folder + code + tests

---
+++ SkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester{
+    @Test
+    public void testSubtract(){
+        assertEquals(4, SkillDemo.subtract(9,4));
+    }
+}
